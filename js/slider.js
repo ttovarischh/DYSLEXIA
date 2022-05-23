1b510dffbb09124bab0e198ae4ac722b4abc1f4b
ttovarischh <polinasot@gmail.com>
2022-05-24 00:40:07 +0300

    slider done

--- js/slider.js
+++ js/slider.js
@@ -1,97 +1,49 @@
-// /* Устанавливаем стартовый индекс слайда по умолчанию: */
-// let slideIndex = 1;
-//
-//
-// $(".curr span").html(slideIndex);
-// /* Вызываем функцию, которая реализована ниже: */
-// showSlides(slideIndex);
-//
-// /* Увеличиваем индекс на 1 — показываем следующий слайд: */
-// function nextSlide() {
-//     showSlides(slideIndex += 1);
-// }
-//
-// /* Уменьшаем индекс на 1 — показываем предыдущий слайд: */
-// function previousSlide() {
-//     showSlides(slideIndex -= 1);
-// }
-//
-// /* Устанавливаем текущий слайд: */
-// function currentSlide(n) {
-//     showSlides(slideIndex = n);
-// }
-//
-// /* Функция перелистывания: */
-// function showSlides(n) {
-//     /* Обращаемся к элементам с названием класса "item", то есть к картинкам: */
-//     let slides = document.getElementsByClassName("item");
-//
-//     /* Проверяем количество слайдов: */
-//     if (n > slides.length) {
-//       slideIndex = 1
-//     }
-//     if (n < 1) {
-//         slideIndex = slides.length
-//     }
-//
-//     /* Проходим по каждому слайду в цикле for: */
-//     for (let slide of slides) {
-//         slide.style.display = "none";
-//     }
-//     /* Делаем элемент блочным: */
-//     slides[slideIndex - 1].style.display = "block";
-// }
-//
-// $("#left").click(previousSlide);
-// $("#right").click(nextSlide);
-
-jQuery(document).ready(function ($) {
-
-  // $('#checkbox').change(function(){
-  //   setInterval(function () {
-  //       moveRight();
-  //   }, 3000);
-  // });
-
 	var slideCount = $('.item').length;
 	var slideWidth = $('.item').width();
-	var slideHeight = $('.item').height();
-	var sliderUlWidth = slideCount * slideWidth;
+	var i = 1
+	var xMove = 200
 
-  $(".curr").html(slideCount);
+	$(".curr").html(i);
   $(".all").html(slideCount);
 
+	function moveLeft() {
+		$('.slides').animate({
+				left: '-='+slideWidth
+		}, 200, function () {
+		});
+	};
 
-	$('#slider').css({ width: slideWidth, height: slideHeight });
+	function moveRight() {
+		$('.slides').animate({
+				left: '+='+slideWidth
+		}, 200, function () {
+		});
+	};
 
-	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+	$('#right').click(function () {
+		if (i == slideCount) {
+			i = 1
+			$('.slides').animate({
+					left: 0
+			}, 200, function () {
+			});
+		} else {
+			i = i + 1;
+			moveLeft();
+		}
+		$(".curr").html(i);
+	});
 
-    $('#slider ul li:last-child').prependTo('#slider ul');
-
-    function moveLeft() {
-        $('#slider ul').animate({
-            left: + slideWidth
-        }, 200, function () {
-            $('#slider ul li:last-child').prependTo('#slider ul');
-            $('#slider ul').css('left', '');
-        });
-    };
-
-    function moveRight() {
-        $('#slider ul').animate({
-            left: - slideWidth
-        }, 200, function () {
-            $('#slider ul li:first-child').appendTo('#slider ul');
-            $('#slider ul').css('left', '');
-        });
-    };
-
-    $('#left').click(function () {
-        moveLeft();
-    });
-
-    $('#right').click(function () {
-        moveRight();
-    });
-
-});
+	$('#left').click(function () {
+		if (i == 1) {
+			i = slideCount
+			$('.slides').animate({
+					left: - (slideWidth * (slideCount - 1))
+			}, 200, function () {
+			});
+		} else {
+			i = i - 1;
+			moveRight();
+		}
+		$(".curr").html(i);
+	});
